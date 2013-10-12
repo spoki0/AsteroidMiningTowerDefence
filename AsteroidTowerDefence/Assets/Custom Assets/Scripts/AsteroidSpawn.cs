b0289184d82451cd2e23e97f1a02bde81145322b
spoki0 <spoki0@hotmail.com>
2013-10-12 18:32:34 +0200

    asteroid spawn now with random angle

--- AsteroidTowerDefence/Assets/Custom Assets/Scripts/AsteroidSpawn.cs
+++ AsteroidTowerDefence/Assets/Custom Assets/Scripts/AsteroidSpawn.cs
@@ -3,10 +3,10 @@ using System.Collections;
 
 public class AsteroidSpawn : MonoBehaviour {
 
-	int BulletSpeed = 10;
-	public Rigidbody asteroid;
+	public Rigidbody[] Asteroids;
 	public Transform[] SpawnPoint;
 	int x = 0;
+	int mod = 70;
 	
 	// Use this for initialization
 	void Start () {
@@ -16,15 +16,16 @@ public class AsteroidSpawn : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		x++;
-		if (x % 180 == 0){
+		if (Random.Range(0,100) > mod && x % 60 == 0){
 			int spawn = Random.Range(0, SpawnPoint.Length);
+			int type = Random.Range(0, Asteroids.Length);
 			
-			Rigidbody instance = Instantiate(asteroid,
+						
+			Rigidbody instance = Instantiate(Asteroids[type],
 											 SpawnPoint[spawn].position,
 											 SpawnPoint[spawn].rotation) as Rigidbody;
 			
-			
-			instance.AddForce(Vector3.right * -Random.Range(50,500));
+			instance.AddForce(-Random.Range(10,100),Random.Range(-50,50),0);
 			
 		}
 	}
