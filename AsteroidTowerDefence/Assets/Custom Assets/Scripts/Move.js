372ad23e055a2c6084fd86f255d95bc7f2194043
JiffyHACC <k_e_s_j@live.no>
2013-10-12 13:16:06 +0200

    Maybe?

--- AsteroidTowerDefence/Assets/Custom Assets/Scripts/Move.js
+++ AsteroidTowerDefence/Assets/Custom Assets/Scripts/Move.js
@@ -1,12 +1,6 @@
-﻿var speed : float = 5.0;
+﻿//test
 
-function Start() 
-{
- var randomDirection : Vector3 = new Vector3(Random.Range(-359, 359),Random.Range(-359, 359), 0);
- transform.Rotate(randomDirection);
-}
- 
 function Update()
 {
-  transform.position += transform.forward * speed * Time.deltaTime;
+  transform.position += new Vector3(0.2,0,0);
 }
