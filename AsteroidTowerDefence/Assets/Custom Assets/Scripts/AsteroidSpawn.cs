b026fcb1ccd1e5b739e0b7f017c8138c8ae2b48a
spoki0 <spoki0@hotmail.com>
2013-10-12 17:01:59 +0200

    starting of a real project

---
+++ AsteroidTowerDefence/Assets/Custom Assets/Scripts/AsteroidSpawn.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidSpawn : MonoBehaviour {
+
+	int BulletSpeed = 10;
+	public Rigidbody asteroid;
+	public Transform[] SpawnPoint;
+	int x = 0;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		x++;
+		if (x % 180 == 0){
+			int spawn = Random.Range(0, SpawnPoint.Length);
+			
+			Rigidbody instance = Instantiate(asteroid,
+											 SpawnPoint[spawn].position,
+											 SpawnPoint[spawn].rotation) as Rigidbody;
+			
+			instance.AddForce(SpawnPoint[spawn].right * -Random.Range(50,500));
+			
+		}
+	}
+}
---
+++ AsteroidTowerDefence/Assets/Custom Assets/Scripts/RotateAround.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAround : MonoBehaviour {
+	
+	public Transform parent;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround(parent.position, new Vector3(0,0,1), Time.deltaTime * 10);
+	}
+}
